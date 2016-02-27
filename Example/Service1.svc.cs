1a02ff014f3957df8620707b4f0d31924a68115e
Felix Martinez Alvaro <Felix.Martinez@tajamar365.com>
2016-02-27 11:14:35 +0100

    Finished

---
+++ Example/Service1.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Example
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        string[] _letters = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T" };
+
+        public string GetLetter(string dni)
+        {
+            var n = Convert.ToInt32(dni);
+            if(n < 0 || n > 99999999)
+                throw new FaultException("Dni proporcionado no valido");
+            return "La letra de su dni es" + _letters[n%23];
+        }
+    }
+}
